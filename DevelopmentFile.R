0590c1724dd553521fe18e242d0ed7d1fbe55223
alexpetriWUSTL <36051450+alexpetriWUSTL@users.noreply.github.com>
2018-03-19 18:14:18 -0400

    Updated development file
    Updated development file to include functionality

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -18,17 +18,22 @@ check(current.code)
 student <- new("Rasch", name = "Jason", a = c(37,43,55,22,34), y = c(0,1,1,0,1))
 theta <- c(48)
 
+#raschModel_Probability functionality
 prob <- raschModel_Probability(raschObj = student, theta = theta)
 prob
 
+#raschModel_Likelihood functionality
 lik <- raschModel_Likelihood(raschObj = student, theta = theta)
 lik
 
+#raschModel_Prior functionality
 prior <- raschModel_Prior(theta = theta)
 prior
 
+#raschModel_EAP functionality
 EAP <- raschModel_EAP(raschObj = student, theta = theta)
 EAP
 
+#print functionality
 print(raschObj = student, theta = theta)
 
